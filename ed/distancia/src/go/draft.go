b531158157fb5b76c744dabe492f86bb2cc89593
joaopedro-creator <joaopedro0744@gmail.com>
2026-05-18 19:44:26 +0000

    novo

--- ed/distancia/src/go/draft.go
+++ ed/distancia/src/go/draft.go
@@ -21,7 +21,46 @@ func isValid(s []byte, pos int, digit byte, L int) bool {
     return true
 }
 
+func backtrack(s []byte, dots []int, idx, L int) bool {
+   
+    if idx == len(dots) {
+        return true
+    }
+
+    pos := dots[idx]
+
+    for d := byte('0'); d <= byte('0'+L); d++ {
+        if isValid(s, pos, d, L) {
+            s[pos] = d                                  
+            if backtrack(s, dots, idx+1, L) {
+                return true                             
+            }
+            s[pos] = '.'                               
+        }
+    }
+
 
 func main() {
     
+    entrada := bufio.NewScanner(os.Stdin)
+    var L int
+    var s string
+
+    for entrada.Scan() {
+        fmt.Sscanf(entrada.Text(), "%d %s", &L, &s)
+        sBytes := []byte(s)
+        var dots []int
+
+        for i, c := range sBytes {
+            if c == '.' {
+                dots = append(dots, i)
+            }
+        }
+
+        if backtrack(sBytes, dots, 0, L) {
+            fmt.Println(string(sBytes))
+        } else {
+            fmt.Println("NO SOLUTION")
+        }
+    }
 }
