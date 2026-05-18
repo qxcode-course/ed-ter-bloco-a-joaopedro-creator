2face48cb066cae68c37a86ec559abe7fdd5e7e0
joaopedro-creator <joaopedro0744@gmail.com>
2026-05-18 20:20:04 +0000

    novo

--- ed/ilhas/src/go/main.go
+++ ed/ilhas/src/go/main.go
@@ -11,9 +11,9 @@ import (
 func numIslands(grid [][]byte) int {
 	count := 0
 
-	var dfs func(int, int)
+	var dfs func(i, j int)
 	dfs = func(i, j int) {
-		if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || grid[i][j] == '0' {
+		if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || grid[i][j] == '1' {
 			return
 		}
 		grid[i][j] = '0' 
@@ -22,16 +22,17 @@ func numIslands(grid [][]byte) int {
 		dfs(i, j-1)       
 		dfs(i, j+1)       
 	}
+	 for i := range grid {
+        for j := range grid[i] {
+            if grid[i][j] == '1' {
+                dfs(i, j) // afunda toda a ilha
+                count++
+            }
+        }
+    }
 
-	for i := 0; i < len(grid); i++ {
-		for j := 0; j < len(grid[0]); j++ {
-			if grid[i][j] == '1' {
-				count++
-				dfs(i, j)
-			}
-		}
+    return count
 	
-	return 0
 }
 
 // Não modifique a função main
