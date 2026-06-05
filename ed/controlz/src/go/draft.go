e4745ebdc70d1f3d9a88b234db72feeacbeafa23
joaopedro-creator <joaopedro0744@gmail.com>
2026-06-05 18:09:42 +0000

    novo

--- ed/controlz/src/go/draft.go
+++ ed/controlz/src/go/draft.go
@@ -4,6 +4,21 @@ import (
     "os"
     "bufio"
 )
+
+
+
+type Estado struct {
+	texto  []rune
+	cursor int
+}
+
+func copia(texto []rune, cursor int) Estado {
+	t := make([]rune, len(texto))
+	copy(t, texto)
+	return Estado{t, cursor}
+}
+
+
 func main() {
      scanner := bufio.NewScanner(os.Stdin)
     scanner.Scan()
@@ -11,7 +26,13 @@ func main() {
 
      texto := []rune{}
     cursor := 0
+
+ 
+    var undoStack []Estado
+    var redoStack []Estado
+    
     for _, ch := range entrada {
+    anterior := copia(texto, cursor)
         switch ch{
     case 'R' :
         texto = append(texto[:cursor], append([]rune{'\n'}, texto[cursor:]...)...)
@@ -26,6 +47,22 @@ func main() {
         if cursor < len(texto) {
             texto = append(texto[:cursor], texto[cursor+1:]...)
         }
+    case 'Z' :
+     if len(undoStack) > 0 {
+            redoStack = append(redoStack, copia(texto, cursor))
+            ultimo := undoStack[len(undoStack)-1]
+            undoStack = undoStack[:len(undoStack)-1]
+            texto, cursor = ultimo.texto, ultimo.cursor
+        }
+        
+    case 'Y' :
+         if len(redoStack) > 0 {
+            undoStack = append(undoStack, copia(texto, cursor))
+            ultimo := redoStack[len(redoStack)-1]
+            redoStack = redoStack[:len(redoStack)-1]
+            texto, cursor = ultimo.texto, ultimo.cursor
+        }
+        
     case '>' :
         if cursor < len(texto) {
             cursor++
@@ -37,6 +74,10 @@ func main() {
     default: 
             texto = append(texto[:cursor], append([]rune{ch}, texto[cursor:]...)...)
             cursor++
+    }
+    if ch != 'Z' && ch != 'Y' && string(anterior.texto) != string(texto) {
+        undoStack = append(undoStack, anterior)
+        redoStack = nil
     }
     }
     texto = append(texto[:cursor], append(texto[cursor:], []rune{'|'}...)...)
