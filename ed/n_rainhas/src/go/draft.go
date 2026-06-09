43421a44cac8f4f95d7b435b0517d892a2450802
joaopedro-creator <joaopedro0744@gmail.com>
2026-06-09 13:58:55 +0000

    novo

--- ed/n_rainhas/src/go/draft.go
+++ ed/n_rainhas/src/go/draft.go
@@ -1,5 +1,38 @@
 package main
 import "fmt"
+
+func rainhas(linha, n int, colunas []int) int {
+    if linha == n {
+        return 1
+    }
+    
+    count := 0
+    for col := 0; col < n; col++ {
+        valido := true
+        for l := 0; l < linha; l++ {
+            if colunas[l] == col || abs(colunas[l]-col) == abs(l-linha) {
+                valido = false
+                break
+            }
+        }
+        if valido {
+            colunas[linha] = col
+            count += rainhas(linha+1, n, colunas)
+        }
+    }
+    return count
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+    var n int 
+    fmt.Scan(&n)
+    colunas := make([]int, n)
+    fmt.Println(rainhas(0, n, colunas))
 }
--- ed/pilhado/src/go/main.go
+++ ed/pilhado/src/go/main.go
@@ -6,3 +6,54 @@ import (
 	"os"
 )
 
+type Pos struct{
+	l, c int
+}
+func dfs(grid [][]rune, start, end Pos) {
+    caminho := NewStack[Pos]()
+    becos := NewStack[Pos]()
+    caminho.Push(start)
+
+    for !caminho.IsEmpty() {
+        atual := caminho.Top()
+        grid[atual.l][atual.c] = '.'
+
+        if atual == end {
+            break
+        }
+
+        vizinhos := []Pos{
+            {atual.l - 1, atual.c},
+            {atual.l + 1, atual.c},
+            {atual.l, atual.c - 1},
+            {atual.l, atual.c + 1},
+        }
+
+        validos := []Pos{}
+        for _, v := range vizinhos {
+            if grid[v.l][v.c] == ' ' || grid[v.l][v.c] == 'F' {
+                validos = append(validos, v)
+            }
+        }
+
+        if len(validos) > 0 {
+            caminho.Push(validos[0])
+        } else {
+            becos.Push(caminho.Pop())
+        }
+    }
+
+    for !becos.IsEmpty() {
+        b := becos.Pop()
+        grid[b.l][b.c] = ' '
+    }
+}
+
+func main() {
+entrada := bufio.NewScanner(os.Stdin)
+entrada.Scan()
+var nl, nc int
+fmt.Sscanf(entrada.Text(), "%d %d", &nl, &nc)
+}
+
+
